e4d2a90b59ff53aae9d347eb4d8180b0e8723268
lest <just.lest@gmail.com>
2011-11-04 10:06:34 +0300

    add input and validate filter

--- runner.rb
+++ runner.rb
@@ -2,4 +2,10 @@ require './ip_filter'
 
 ip_filter = IpFilter.new File.open('ranges.txt')
 
-ip_filter.check('127.0.0.1')
+input = File.read('input.txt').lines.map do |line|
+  [line.strip[1..-1], line[0].chr == '+']
+end
+
+input.each do |ip, expected|
+  puts "#{ip} should#{expected ? '' : ' not'} be matched" if ip_filter.check(ip) != expected
+end
